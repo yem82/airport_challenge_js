b54e577738bc326198ce7c136018fc05c9b653ad
Yemi <yemiadebayo60@gmail.com>
2019-05-29 19:02:27 +0100

    Refactor code

--- spec/airport_spec.js
+++ spec/airport_spec.js
@@ -24,8 +24,14 @@ describe('Airport',function(){
         airport.land(plane);
         expect(airport.planes).toEqual([plane]);
       });
-
     });
+
+      it ('raises an error when airport is full', function() {
+        for (i = 0; i < 50; i++) {
+              airport.land(plane);
+            }
+        expect(function(){ airport.land(plane); }).toThrow('Airport is full!');
+      });
 
     describe ('take off', function() {
 
@@ -33,15 +39,6 @@ describe('Airport',function(){
         airport.land(plane);
         airport.take_off(plane);
         expect(airport.planes).not.toContain(plane);
-      });
-    });
-
-    describe ('airport capacity', function() {
-      it ('raises an error when airport is full', function() {
-        for (i = 0; i < 50; i++) {
-          airport.land(plane);
-        }
-        expect(function(){ airport.land(plane);}).toThrow('Airport is full!');
       });
     });
   });
